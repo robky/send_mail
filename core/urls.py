400ee9b8a277c951f8296a2468c5397c94fcc086
Pavel Gaidukov <mailgpn@gmail.com>
2023-02-12 11:28:22 +0500

    countdown test

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ app_name = 'core'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add/$', views.add, name='add'),
+    url(r'^add-eta/$', views.add_eta, name='add_eta'),
     url(r'^done/$', views.done, name='done'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -47,3 +47,26 @@ def done(request):
         'title': title,
     }
     return render(request, template, context)
+
+
+def add_eta(request):
+    if request.method == 'POST':
+        form = TwoNumbersForm(request.POST)
+        if form.is_valid():
+            one = form.cleaned_data['one']
+            two = form.cleaned_data['two']
+            tasks.add.delay((one, two), countdown=0.2 * 60)
+            return HttpResponseRedirect('/done/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TwoNumbersForm()
+
+    template = 'core/add.html'
+    title = 'Отложенное сложение двух чисел'
+    context = {
+        'title': title,
+        'form': form,
+    }
+
+    return render(request, template, context)
